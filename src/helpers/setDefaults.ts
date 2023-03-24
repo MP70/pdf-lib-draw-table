95324ebeee3548127b88ee3806e59ff8184f10f1
Matt <matt@caliu.co.uk>
2023-03-24 03:03:54 +0000

    tidy def style

--- src/helpers/setDefaults.ts
+++ src/helpers/setDefaults.ts
@@ -26,7 +26,7 @@ export const setDefaults = (
     header: {
       hasHeaderRow: options.header?.hasHeaderRow ?? true, // Set default hasHeaderRow or use provided value
       font: options.header?.font ?? embeddedTableTitleFont, // Set default header font or use provided value
-      textSize: options.header?.textSize ?? 15, // Set default header text size or use provided value
+      textSize: options.header?.textSize ?? 14, // Set default header text size or use provided value
       textColor: options.header?.textColor ?? rgb(0.996, 0.996, 0.996), // Set default header text color or use provided value
       backgroundColor: options.header?.backgroundColor ?? rgb(0, 0.2, 0.4), // Set default header background color or use provided value
       contentAlignment: options.header?.contentAlignment ?? "left", // Set default header content alignment or use provided value
@@ -43,11 +43,11 @@ export const setDefaults = (
       width: options.border?.width ?? 1, // Set default border width or use provided value
     },
     pageMargin: {
-      bottom: options.pageMargin?.bottom ?? 5, // Set default page margin bottom or use provided value
+      bottom: options.pageMargin?.bottom ?? 50, // Set default page margin bottom or use provided value
       right: options.pageMargin?.right ?? 50, // Set default page margin right or use provided value
     },
     contentMargin: {
-      horizontal: options.contentMargin?.horizontal ?? 2, // Set default content margin horizontal or use provided value
+      horizontal: options.contentMargin?.horizontal ?? 3, // Set default content margin horizontal or use provided value
       vertical: options.contentMargin?.vertical ?? 0, // Set default content margin vertical or use provided value
     },
     fillUndefCells: options?.fillUndefCells ?? true,
